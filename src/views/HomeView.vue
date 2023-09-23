31034fb072b1ae8c666de35e94f2f039f91ccce9
mikemrg17 <miguelangelmurga1234@gmail.com>
2023-09-23 01:47:36 -0600

    feat(bootstrap): Bootstrap 5 set up and implementing a navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Hello</h1>
   </main>
 </template>
+
+<script setup lang="ts"></script>
